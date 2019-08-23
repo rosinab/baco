8991fd70cbbcab363799f3f7a67e7d19fdfedd8a
rosinab <51364454+rosinab@users.noreply.github.com>
2019-08-23 13:56:24 +0100

    Added Comments

--- index.php
+++ index.php
@@ -46,7 +46,152 @@
         <p class="servicemain">Lorem ipsum dolor sit amet, consectetur <br>adipicising elit. Maecenas porta auctor velit, quis<br> lacinia snim comdimentum nec.</p>
         <button class="specquote">Get A Quote &rarr;</button>
 </div>
+</div><html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+<link rel="stylesheet" href="main.css">
+<script src="https://kit.fontawesome.com/4eb2121a38.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- HEADER -->
+<?php include 'header.php' ?>
+
+<!-- CAROUSEL AND UNDER -->
+
+<div class="caro">
+<div class="banner" style="background-image: url('img/caro.png'); background-repeat: no-repeat; background-position: center; background-size: cover;">
+<div class="bannertext">  
+<h2>Headline To Go Here</h2>
+    <p class="carotext">Lorem ipsum dolor sit amet, consectetur adipicising elit. Maecenas porta auctor velit, quis lacinia snim comdimentum nec. Maecenas porta auctor.</p>
+    <a class="carobutt">Get A Quote &rarr;</a>
+    </div>
 </div>
+    <div class="banner" style="background-image: url('img/caro1.png'); background-repeat: no-repeat; background-position: center; background-size: cover;">  
+    <div class="bannertext"> 
+    <h2>Headline To Go Here</h2>
+    <p class="carotext">Lorem ipsum dolor sit amet, consectetur adipicising elit. Maecenas porta auctor velit, quis lacinia snim comdimentum nec. Maecenas porta auctor.</p>
+    <a class="carobutt">Get A Quote &rarr;</a> 
+</div>
+</div>
+</div>
+
+<div class="undercarousel">
+    <h5 class="titletext">Total Recycling & Waste Management Solutions</h5>
+</div>
+
+<!-- SERVICES -->
+
+<h3 class="servicestitle">Our Services</h3>
+<div class="services">
+    <div class="skip">
+    <div class="titleplusimage">
+    <img src="img/skip.png" class="img">
+        <h4>Skip Hire</h4>
+        </div>
+        <div class="iefix">
+        <p class="servicemain">Lorem ipsum dolor sit amet, consectetur <br>adipicising elit. Maecenas porta auctor velit, quis<br> lacinia snim comdimentum nec.</p>
+        <a class="specquote">Get A Quote &rarr;</a>
+        <br>
+        <hr>
+</div>
+</div>
+    <div class="comwaste">
+    <div class="titleplusimage">
+    <img src="img/truck.png" class="img">
+        <h4>Commercial Waste</h4>
+        </div>
+        <div class="iefix">
+        <p class="servicemain">Lorem ipsum dolor sit amet, consectetur <br>adipicising elit. Maecenas porta auctor velit, quis<br> lacinia snim comdimentum nec.</p>
+        <a class="specquote">Get A Quote &rarr;</a>
+        <br>
+        <hr>
+</div>
+</div>
+<div class="recycle">
+<div class="titleplusimage">
+    <img src="img/recycle.png" class="img">
+        <h4>Recycling</h4>
+        </div>
+        <div class="iefix">
+        <p class="servicemain">Lorem ipsum dolor sit amet, consectetur <br>adipicising elit. Maecenas porta auctor velit, quis<br> lacinia snim comdimentum nec.</p>
+        <a class="specquote">Get A Quote &rarr;</a> 
+        <br>
+        <hr>
+</div>  
+</div>
+</div>
+
+<!-- ABOUT -->
+
+<div class="about">
+<h3 class="abouthead">Welcome to Baco-Compak</h3>
+<p class="headline">Baco Compak has been in the waste removal business for more than 40 years. In that time, we've learned how to determine precisely what it is that our clients require.</p>
+<p class="boxtext">Whether you're in need of a one-off waste removal service for an upcoming project, or you would like to arrange regular service in the King's Lynn area, the team at Baco Compak are ready to assist.</p>
+<a class="aboutbutton">Read More &rarr;</a>    
+</div>
+
+<!-- NEWS -->
+
+<h3 class="servicestitle">Latest News</h3>
+<div class="latest">
+    <div class="article">
+    <img src="img/car1.png" class="tab newspic">
+    <p class="date"><i class="far fa-calendar-alt"></i> 22/07/19</p>
+    <h4>Headline 1</h4>
+    <p class="mobp">Whether you're in need of a one-off waste removal service for an upcoming project, or you.</p>
+    <a class="maina">Read More &rarr;</a>
+    <br>
+        <hr>
+</div>
+    <div class="article">
+    <img src="img/car2.png" class="tab newspic">
+    <p class="date"><i class="far fa-calendar-alt"></i> 22/07/19</p>
+    <h4>Headline 2</h4>
+    <p class="mobp">Whether you're in need of a one-off waste removal service for an upcoming project, or you...</p>
+    <a class="maina">Read More &rarr;</a>
+    <br>
+        <hr>
+</div>
+<div class="article mobart">
+    <img src="img/car3.png" class="tab">
+    <p><i class="far fa-calendar-alt"></i> 22/07/19</p>
+    <h4>Headline 3</h4>
+    <p>Whether you're in need of a one-off waste removal service for an upcoming project, or you...</p>
+    <a class="maina">Read More &rarr;</a>
+    <br>
+        <hr>
+</div>
+<div class="article mobart">
+    <img src="img/car4.png" class="tab">
+    <p><i class="far fa-calendar-alt"></i> 22/07/19</p>
+    <h4>Headline 4</h4>
+    <p>Whether you're in need of a one-off waste removal service for an upcoming project, or you...</p>
+    <a class="maina">Read More &rarr;</a>
+    <br>
+        <hr>
+</div>
+</div>
+
+<!-- FOOTER -->
+
+<?php include 'footer.php' ?>
+
+<!-- JAVASCRIPT -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="slick/slick.min.js"></script>
+<script src="site.js"></script>
+</body>
+
+</html>
     <div class="comwaste">
     <div class="titleplusimage">
     <img src="img/truck.png" class="img">
